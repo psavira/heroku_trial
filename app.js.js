4f751a0e2d891e9ce44a6286b6ba7537d9c44a72
psavira <savi2586@stthomas.edu>
2022-02-09 18:57:29 -0600

    first commit

---
+++ app.js.js
@@ -0,0 +1,13 @@
+//const res = require("express/lib/response");
+
+const app = require("express")();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("",(req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(PORT,() => {
+    console.log('App up at port ${PORT}');
+});
