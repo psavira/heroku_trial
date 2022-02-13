d081a7f628f755c0e16d7355fcc25c846c0b1f3b
psavira <savi2586@stthomas.edu>
2022-02-13 11:18:19 -0600

    first try login page

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //const res = require("express/lib/response");
 
 const app = require("express")();
-
+//const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
-app.get("",(req, res) => {
-    res.send("Hello world! I hope youre're doing well");
+app.get("/",(req, res) => {
+    // res.send("Hello world! I hope youre're doing well");
+    res.sendFile(__dirname + "/login-page.html");
 });
 
 app.listen(PORT,() => {
-    console.log('App up at port ${PORT}');
+    console.log('App up at port %s', PORT);
 });
